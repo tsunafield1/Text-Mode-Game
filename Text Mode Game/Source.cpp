55d81078a44f9ff87a21863967c51505c3473f12
GLIT RUNGROJKIJKUL <63010022@KMITL.AC.TH>
2020-09-17 18:14:52 +0700

    TextModeGame02

--- Text Mode Game/Source.cpp
+++ Text Mode Game/Source.cpp
@@ -5,12 +5,15 @@ void draw_ship(int, int);
 void erase_ship(int, int);
 void setcursor(bool);
 void setcolor(int , int);
-
+void draw_bullet(int, int);
+void erase_bullet(int, int);
 
 int main()
 {
+	bool b[5] = { 0,0,0,0,0 };
 	char ch = ' ';
-	int x = 30, y = 18,direc = 0;
+	int x = 40, y = 18,direc = 0;
+	int bx[5], by[5];
 	setcursor(0);
 	draw_ship(x,y);
 	do
@@ -18,40 +21,62 @@ int main()
 		if (_kbhit())
 		{
 			ch = _getch();
+			if (ch == ' ')
+			{
+				for (int i = 0; i < 5; i++)
+				{
+					if (!b[i])
+					{
+						b[i] = 1;
+						bx[i] = x+3;
+						by[i] = y-1;
+						break;
+					}
+				}
+			}
 			if (ch == 'a')
 			{
 				direc = 1;
-				/*erase_ship(x, y);
-				draw_ship(--x, y);*/
 			}
 			if (ch == 'd')
 			{
 				direc = 2;
-				/*erase_ship(x, y);
-				draw_ship(++x, y);*/
-			}
-			if (ch == 'w' && y>0)
-			{
-				/*erase_ship(x, y);
-				draw_ship(x, --y);*/
 			}
 			if (ch == 's')
 			{
 				direc = 0;
-				/*erase_ship(x, y);
-				draw_ship(x, ++y);*/
 			}
-			//fflush(stdin);
+			fflush(stdin);
 		}
-		if (direc == 1 && x > 0)
+		for (int i = 0; i < 5; i++)
 		{
-			erase_ship(x, y);
-			draw_ship(--x, y);
+			if (b[i])
+			{
+				if (by[i] == 0)
+				{
+					erase_bullet(bx[i], by[i]);
+					b[i] = 0;
+				}
+				else
+				{
+					erase_bullet(bx[i], by[i]);
+					draw_bullet(bx[i], --by[i]);
+				}
+			}
 		}
-		if (direc == 2 && x < 72)
+		if (direc != 0)
 		{
-			erase_ship(x, y);
-			draw_ship(++x, y);
+			if (direc == 1)
+			{
+				erase_ship(x, y);
+				draw_ship(--x, y);
+			}
+			if (direc == 2)
+			{
+				erase_ship(x, y);
+				draw_ship(++x, y);
+			}
+			if (x == 0 || x == 72) direc = 0;
 		}
 		Sleep(100);
 	} while (ch != 'x');
@@ -87,4 +112,20 @@ void setcolor(int fg, int bg)
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
+
+void draw_bullet(int x, int y)
+{
+	COORD c = { x , y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	setcolor(15, 0);
+	printf("o");
+}
+
+void erase_bullet(int x, int y)
+{
+	COORD c = { x , y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	setcolor(0, 0);
+	printf(" ");
 }
